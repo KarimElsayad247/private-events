f1073f91217a3f47c123210a5857e758926b63f9
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-11 18:48:30 +0200

    Create basics for creating events, with styling
    Add a form to create event for authenticated users
    
    Style events index page as a grid

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+class EventsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.organized_events.build(event_params)
+    if @event.save
+      flash[:notice] = "Event created!"
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :location, :date)
+  end
+end
