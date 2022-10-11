f6e1a9810d61a84348bd83ea576547383bee3ff8
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-11 15:18:04 +0200

    Add username validation
    For now that's the only validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :organized_events, foreign_key: "organizer_id", class_name: "Event"
+
+  validates :username, presence: true, uniqueness: true,
+            length: { in: 3..20 }
 end
