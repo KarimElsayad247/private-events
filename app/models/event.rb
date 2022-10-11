17a3d3fbae7423b13f012705c4afa11ace7cc0df
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-11 15:36:31 +0200

    Add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :organizer, class_name: "User"
+
+  validates :title, :date, :location, :organizer_id, presence: true
+  validate :date_can_not_be_in_the_past
+
+  def date_can_not_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
 end
+
